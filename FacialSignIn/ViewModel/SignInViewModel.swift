bb54b875d17fcbc1ce00e02e8144dab13b789432
Yi JIANG <jiang.yi@siphty.com>
2018-11-02 16:15:45 +1100

    Update: keep updating

--- FacialSignIn/ViewModel/SignInViewModel.swift
+++ FacialSignIn/ViewModel/SignInViewModel.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+import UIKit
+
+class SignInViewModel {
+    
+    let apiClient: ApiClient
+    
+    var potentialVisitor: PotentialVisitors? {
+        didSet {
+            didUpdatePotentialVisitors?()
+        }
+    }
+    var visitor: Visitor? {
+        didSet {
+            updateVisitorForm?()
+        }
+    }
+    
+    var alertMessage: String? {
+        didSet {
+            showAlert?()
+        }
+    }
+    
+    // MARK: - UI Binding
+    var textOfNameTextField: String {
+        return visitor?.name ?? ""
+    }
+    var textOfMobileTextField: String {
+        return visitor?.mobile ?? ""
+    }
+    var textOfEmailTextField: String {
+        return visitor?.email ?? ""
+    }
+    var imageOfVisitorImage: UIImage {
+        #warning("The visitor.image is a long string (.jpg). We need decode it into uiimage.")
+        return UIImage()
+    }
+    var textOfHosterNameTextField: String {
+        return visitor?.visiting ?? ""
+    }
+    
+    
+    // MARK: - Events
+    var didUpdatePotentialVisitors: (() -> Void)?
+    var showAlert: (() -> Void)?
+    var updateVisitorForm: (() -> Void)?
+    
+    init(_ apiClient: ApiClient = ApiClient()) {
+        self.apiClient = apiClient
+    }
+    
+    
+}
