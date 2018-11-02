3729d9dad040f8ab4f348a79da44f01fe26f69c0
Yi JIANG <jiang.yi@siphty.com>
2018-11-02 16:32:34 +1100

    Update: half way done for the SignInViewModel.

--- FacialSignIn/ViewModel/SignInViewModel.swift
+++ FacialSignIn/ViewModel/SignInViewModel.swift
@@ -30,6 +30,12 @@ class SignInViewModel {
         }
     }
     
+    var isLoading = false {
+        didSet {
+            updateLoadingStatus?()
+        }
+    }
+    
     // MARK: - UI Binding
     var textOfNameTextField: String {
         return visitor?.name ?? ""
@@ -50,13 +56,40 @@ class SignInViewModel {
     
     
     // MARK: - Events
+    var hasError: ((RequestError) -> Void)?
     var didUpdatePotentialVisitors: (() -> Void)?
     var showAlert: (() -> Void)?
     var updateVisitorForm: (() -> Void)?
+    var updateLoadingStatus: (() -> Void)?
     
     init(_ apiClient: ApiClient = ApiClient()) {
         self.apiClient = apiClient
     }
     
+    func createNewVisitor() {
+        #warning("Call new visitor API to create new visitor")
+        isLoading = true
+        apiClient.networkRequest(.newVisitor) { [weak self] (data, error) in
+            guard let self = self else { return }
+            self.isLoading = false
+            if let error = error {
+                self.hasError?(error)
+                self.alertMessage = error.errorDescription
+                return
+            }
+            if let data = data {
+                do {
+                    let decoder = JSONDecoder()
+                    self.visitor = try decoder.decode(Visitor.self, from: data)
+                } catch let error {
+                    self.alertMessage = "Can't decode API response: \(error.localizedDescription)"
+                }
+        }
+    }
     
+    func recogniseVisitor() {
+        #warning("Wrap 5 face images into API call's body, backend service will train it self.")
+    }
+        
+        
 }
